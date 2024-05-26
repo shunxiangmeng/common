eb91da5620b2436d488e6c6acbec2a98456b6751
ShawnMeng <30586235+shunxiangmeng@users.noreply.github.com>
2024-05-26 22:58:41 +0800

    add

---
+++ middleware/private/include/PrivClient.h
@@ -0,0 +1,10 @@
+/************************************************************************
+ * Copyright(c) 2024 shanghai ulucu technology
+ * 
+ * File        :  PrivClient.h
+ * Author      :  mengshunxiang 
+ * Data        :  2024-05-26 22:57:52
+ * Description :  None
+ * Note        : 
+ ************************************************************************/
+#pragma once
