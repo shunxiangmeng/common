ed1462b2f2eea67a3b94eaa50eb95235cd3358d9
shunxiangmeng <shunxiangmeng@gmail.com>
2024-07-30 20:05:55 +0800

    debug

--- hal/Defines.h
+++ hal/Defines.h
@@ -9,6 +9,7 @@
  ************************************************************************/
 #pragma once
 #include <stdint.h>
+#include <vector>
 
 typedef enum : int32_t {
     IMAGE_PIXEL_FORMAT_RGB_888 = 0,
@@ -23,17 +24,29 @@ typedef enum {
     E_TargetType_body
 } TargetType;
 
+typedef enum {
+    E_TargetShapType_rect,
+    E_TargetShapType_point_pose,
+} TargetShapType;
+
 typedef struct {
-    float x;      //0-1
-    float y;      //0-1
+    float x;      //0 -> 1.0f
+    float y;      //0 -> 1.0f
     float w;
     float h;
 } Rect;
 
 typedef struct {
+    float x;
+    float y;
+} Point;
+
+typedef struct {
+    TargetShapType shap_type;
     TargetType type;
     uint32_t id;
     Rect rect;
+    std::vector<Point> points;
 } Target;
 
 typedef struct {
